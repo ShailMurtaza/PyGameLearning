4f56d8046b1f3dc95bfd02ca647f9d31fc14b272
ShailMurtaza <shailmurtaza@gmail.com>
2024-08-23 14:00:00 +0500

    4.rotating_cube: Function to rotate in all directions in 3D space

--- 4.rotating_cube.py
+++ 4.rotating_cube.py
@@ -12,6 +12,13 @@ class COLORS:
     blue = (31, 81, 255)
     orange = (255, 172, 28)
     white = (255, 255, 255)
+    yellow = (255, 255, 0)
+    pink = (255, 192, 203)
+    teal = (0, 128, 128)
+    cyan = (0, 255, 255)
+    indigo = (75, 0, 130)
+    purple = (128, 0, 128)
+    magenta = (255, 0, 255)
 
 # Points for CUBE
 POINTS = [
@@ -27,9 +34,9 @@ POINTS = [
 ]
 
 EDGES = (
-    (0, 1), (1, 2), (2, 3), (3, 0), # Square 1
-    (4, 5), (5, 6), (6, 7), (7, 4), # Square 2
-    (0, 4), (1, 5), (2, 6), (3, 7), # Join both Squares
+    (0, 1, COLORS.teal), (1, 2, COLORS.green), (2, 3, COLORS.magenta), (3, 0, COLORS.red), # Square 1
+    (4, 5, COLORS.cyan), (5, 6, COLORS.blue), (6, 7, COLORS.pink), (7, 4, COLORS.yellow), # Square 2
+    (0, 4, COLORS.indigo), (1, 5, COLORS.purple), (2, 6, COLORS.orange), (3, 7, COLORS.white), # Join both Squares
 )
 
 pygame.init()
@@ -58,13 +65,6 @@ def draw_points(vertices, color):
             pygame.draw.circle(WIN, color, v, 2)
 
 
-def draw_lines(points, color):
-    for i in range(len(points)):
-        p1 = points[i]
-        p2 = points[(i+1) % len(points)]
-        pygame.draw.line(WIN, color, p1, p2)
-
-
 # Get 3D vertices and return 2D perpective projection
 def projection(vertex, f):
     x, y, z = vertex
@@ -82,18 +82,34 @@ def map_to_screen(x, y):
     return (screen_x, screen_y)
 
 
+def rotate_x(vertex, angle):
+    x, y, z = vertex
+    new_y = y * math.cos(angle) + z * math.sin(angle)
+    new_z = -y * math.sin(angle) + z * math.cos(angle)
+    return [x, new_y, new_z]
+
 def rotate_y(vertex, angle):
     x, y, z = vertex
-    new_x = x * math.cos(angle) + z * math.sin(angle)
-    new_z = -x * math.sin(angle) + z * math.cos(angle)
+    new_x = x * math.cos(angle) - z * math.sin(angle)
+    new_z = x * math.sin(angle) + z * math.cos(angle)
     return [new_x, y, new_z]
 
+def rotate_z(vertex, angle):
+    x, y, z = vertex
+    new_x = x * math.cos(angle) + y * math.sin(angle)
+    new_y = -x * math.sin(angle) + y * math.cos(angle)
+    return [new_x, new_y, z]
 
-def rotate_points(points, angle):
+def rotate_points(points, angle, axis):
     angle = math.radians(angle)
     shape_rotated = []
     for i in points:
-        point_rot = rotate_y(i, angle)
+        if axis == "x":
+            point_rot = rotate_x(i, angle)
+        elif axis == "y":
+            point_rot = rotate_y(i, angle)
+        else:
+            point_rot = rotate_z(i, angle)
         shape_rotated.append(point_rot)
     return shape_rotated
 
@@ -102,7 +118,6 @@ def translate_z(points, offset_z):
     for x, y, z in points:
         translated_points.append([x, y, z + offset_z])
     return translated_points
-
 
 f = 1 # Focal Length
 def main():
@@ -122,8 +137,9 @@ def main():
 
         # Draw EDGES of CUBE
         for i in EDGES:
-            pygame.draw.line(WIN, COLORS.white, v2d[i[0]], v2d[i[1]])
-        POINTS = rotate_points(POINTS, -1 * dt * 70) # Rotate every point by 1 degree after every iteration
+            pygame.draw.line(WIN, i[2], v2d[i[0]], v2d[i[1]], 2)
+        POINTS = rotate_points(POINTS, 1 * dt * 70, "y") # Rotate every point by 1 degree after every iteration
+        POINTS = rotate_points(POINTS, 0.5 * dt * 70, "z") # Rotate every point by 1 degree after every iteration
 
         draw_win()
         events()
