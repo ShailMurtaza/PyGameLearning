f01c9485232ae3c8e9c116fb741d845013b339e3
ShailMurtaza <shailmurtaza@gmail.com>
2025-02-23 12:22:23 +0500

    Events: Better key handling

--- 3D_Renderer/events.py
+++ 3D_Renderer/events.py
@@ -1,17 +1,34 @@
 import pygame
 
+class Key:
+    def __init__(self, key, ):
+        self.pressed = False
+        self.key = key
+
+
 # If key is pressed or not
 KEYS = {
-    "W": False,
-    "A": False,
-    "S": False,
-    "D": False,
+    "W": (Key(pygame.K_w), "y", 1),
+    "A": (Key(pygame.K_a), "x", -1),
+    "S": (Key(pygame.K_s), "y", -1),
+    "D": (Key(pygame.K_d), "x", 1),
+    "Q": (Key(pygame.K_q), "z", 1),
+    "E": (Key(pygame.K_e), "z", -1),
+
+    "I": (Key(pygame.K_i), "rotate_x", 4),
+    "K": (Key(pygame.K_k), "rotate_x", -4),
+    "L": (Key(pygame.K_l), "rotate_y", 4),
+    "J": (Key(pygame.K_j), "rotate_y", -4),
+    "U": (Key(pygame.K_u), "rotate_z", 4),
+    "O": (Key(pygame.K_o), "rotate_z", -4),
 }
 
 
 # Transformations
 TRANSFORMATION = {
-    "angle": 0.0,
+    "rotate_x": 0.0,
+    "rotate_y": 0.0,
+    "rotate_z": 0.0,
     "x": 0.0,
     "y": 0.0,
     "z": 0.0
@@ -22,35 +39,22 @@ TRANSFORMATION = {
 def events(dt):
     run = True
     transformation_factor = 5
-    if (KEYS["A"]):
-        TRANSFORMATION["x"] -= transformation_factor * dt
-    if (KEYS["D"]):
-        TRANSFORMATION["x"] += transformation_factor * dt
-    if (KEYS["W"]):
-        TRANSFORMATION["y"] += transformation_factor * dt
-    if (KEYS["S"]):
-        TRANSFORMATION["y"] -= transformation_factor * dt
+    
+    for _, (key, axis, direction) in KEYS.items():
+        if key.pressed:
+            TRANSFORMATION[axis] += direction * transformation_factor * dt
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             run = False
         elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_a:
-                KEYS["A"] = True
-            elif e.key == pygame.K_d:
-                KEYS["D"] = True
-            elif e.key == pygame.K_w:
-                KEYS["W"] = True
-            elif e.key == pygame.K_s:
-                KEYS["S"] = True
+            for _, (key, axis, direction) in KEYS.items():
+                if (e.key == key.key):
+                    key.pressed = True
+
         elif e.type == pygame.KEYUP:
-            if e.key == pygame.K_a:
-                KEYS["A"] = False
-            elif e.key == pygame.K_d:
-                KEYS["D"] = False
-            elif e.key == pygame.K_w:
-                KEYS["W"] = False
-            elif e.key == pygame.K_s:
-                KEYS["S"] = False
+            for _, (key, axis, direction) in KEYS.items():
+                if (e.key == key.key):
+                    key.pressed = False
         
         elif e.type == pygame.MOUSEWHEEL:
             if e.y == 1:
--- 3D_Renderer/main.py
+++ 3D_Renderer/main.py
@@ -13,14 +13,13 @@ FONT = freetype.SysFont(pygame.font.get_default_font(), 20)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
-# POINTS, EDGES = parse_obj_to_numpy("cube.obj")
+POINTS, EDGES = parse_obj_to_numpy("cube.obj")
 # POINTS, EDGES = parse_obj_to_numpy("sphere.obj")
 # POINTS, EDGES = parse_obj_to_numpy("teapot.obj")
-POINTS, EDGES = parse_obj_to_numpy("airboat.obj")
+# POINTS, EDGES = parse_obj_to_numpy("airboat.obj")
 # POINTS, EDGES = parse_obj_to_numpy("13463_Australian_Cattle_Dog_v3.obj")
 
 POINTS = center_object(POINTS)
-
 cam = (0, 0, 0)
 
 def main():
@@ -31,10 +30,11 @@ def main():
         new_time = time()
         dt = new_time - prev_time
         prev_time = new_time
-        TRANSFORMATION["angle"] += 1
 
-        new_points = POINTS
-        new_points = rotate_y(new_points, TRANSFORMATION["angle"]) # Rotate every point by 1 degree after every iteration
+        new_points = center_object(POINTS)
+        new_points = rotate_x(new_points, TRANSFORMATION["rotate_x"])
+        new_points = rotate_y(new_points, TRANSFORMATION["rotate_y"])
+        new_points = rotate_z(new_points, TRANSFORMATION["rotate_z"])
         new_points = translate(new_points, TRANSFORMATION["x"], TRANSFORMATION["y"], TRANSFORMATION["z"]) # Translated on z-axis to make object smaller and fit in frustum
         new_points = translate(new_points, -cam[0], -cam[1], -cam[2])
 
